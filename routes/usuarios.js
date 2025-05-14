e379e1e70b87ef5c9be087010a14c8f867cc1101
Kendall Gomez <95137012+Kendallgomez1107@users.noreply.github.com>
2025-05-13 23:50:35 -0600

    Update usuarios.js

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -13,6 +13,26 @@ router.get('/', async (req, res) => {
     res.status(500).send('Error del servidor');
   }
 });
+// Obtener un usuario por su ID
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const result = await pool.query(
+      'SELECT * FROM usuarios WHERE id_usuario = $1',
+      [id]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: 'Usuario no encontrado' });
+    }
+
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Error del servidor');
+  }
+});
+
 
 // Crear un nuevo usuario
 router.post('/', async (req, res) => {
